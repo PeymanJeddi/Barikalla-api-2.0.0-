0752f4a6d450f697739e038ee2db8863a45b0630
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-05-27 06:30:19 +0330

    chore: publish swagger config, init swagger'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,29 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\OpenApi(
+ *  @OA\Info(
+ *      title="Barikalla",
+ *      version="0.0.1",
+ *      description="API documentation for Barikalla",
+ *      @OA\Contact(
+ *          email="iman_sp@yahoo.com"
+ *      )
+ *  ),
+ *    @OA\Server(
+ *      description=L5_SWAGGER_APP_NAME,
+ *      url=L5_SWAGGER_CONST_HOST
+ *  ),
+ *  @OA\PathItem(
+ *      path="/"
+ *  )
+ * ),
+ * @OA\Tag(
+ *   name="Auth",
+ *   description="Authenticating"
+ * ),
+ */
 abstract class Controller
 {
     //
