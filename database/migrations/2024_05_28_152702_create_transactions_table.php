2b43cd04d4be7dcc0f0011fac4ee45bd5e1f400e
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-05-31 13:48:58 +0330

    chore: add transaction migration, model and type enum

---
+++ database/migrations/2024_05_28_152702_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('streamer_id')->nullable();
+            $table->integer('amount')->nullable();
+            $table->string('fullname')->nullable();
+            $table->string('mobile', 11);
+            $table->text('description')->nullable();
+            $table->boolean('is_paid')->default(0);
+            $table->string('type')->comment('donate|charge|subscription');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
