476f8e3a9b145b6f8b108cc598f7a53236436757
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-05-31 23:52:44 +0330

    chore: modify transaction service and prevent a bug

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -25,20 +25,47 @@ class TransactionService extends Service
         return $paymentUrl;
     }
 
+    private static function generateOrderId(): int
+    {
+        do {
+            $code = self::generateRandomNumber(); 
+        } while (self::isOrderIdExists($code));
+
+        return $code;
+    }
+
+    private static function isOrderIdExists($orderId): bool
+    {
+        return Transaction::where('order_id', $orderId)->exists();
+    }
+
+    private static function generateRandomNumber(): int
+    {
+        return mt_rand(1000000, 9999999) . mt_rand(1000000, 9999999);
+    }
+
     private static function makePayment(User $user, int $amount, Transaction $transaction): string
     {
-        // Your specified fields for the payment gateway
-        $data = [
-            "CorporationPin" => config('app.payment_corporation_pin'),
-            "Amount" => $amount * 10, // Assuming you need to convert to Rials for the API
-            "OrderId" => $transaction->id, // Generating a unique order ID; adapt as necessary
-            "CallBackUrl" => "http://localhost:8000/api/payment/verify",
-            "AdditionalData" => "test",
-            "Originator" => $user->mobile,
-        ];
+ 
 
         try {
-            $response = Http::post('https://pna.shaparak.ir/mhipg/api/Payment/NormalSale', $data);
+            do {
+                $orderId = self::generateOrderId();
+                $data = [
+                    "CorporationPin" => config('app.payment_corporation_pin'),
+                    "Amount" => $amount * 10,
+                    "OrderId" => $orderId,
+                    "CallBackUrl" => "http://localhost:8000/api/payment/verify",
+                    "AdditionalData" => "test",
+                    "Originator" => $user->mobile,
+                ];
+                $response = Http::post('https://pna.shaparak.ir/mhipg/api/Payment/NormalSale', $data);
+            } while ($response->json()['status'] == -112);
+            
+            $transaction->update([
+                'order_id' => $orderId,
+            ]);
+
         } catch (\Exception $e) {
             abort(500, $e->getMessage());
         }
@@ -51,28 +78,29 @@ class TransactionService extends Service
         }
     }
 
-    public static function verifyTransaction(string $token, Transaction $transaction,)
+    public static function verifyTransaction(string $token, Transaction $transaction)
     {
         $corporationPin = config('app.payment_corporation_pin');
 
         try {
-            $response = Http::post('https://pna.shaparak.ir/mhipg/api/Payment/confirm', [
+            Http::post('https://pna.shaparak.ir/mhipg/api/Payment/confirm', [
                 'CorporationPin' => $corporationPin,
                 'Token' => $token
             ]);
         } catch (\Exception $e) {
             abort(500, $e->getMessage());
         }
-        $responseData = $response->json();
 
         $transaction->update([
             'is_paid' => 1,
         ]);
 
-        $transaction->payment()->create([
+        $payment = $transaction->payment()->create([
             'token' => $token,
             'code' => 'test',
         ]);
-        Log::info($responseData);
+
+        WalletService::chargeWallet($transaction->streamer, $transaction->amount);
+        return $payment;
     }
 }
