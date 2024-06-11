fef4f0246342b5c26c6b9c9d98db3bca900ffc1d
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-06-11 17:35:13 +0330

    chore: calculate wage, tax on charge wallet for streamer

--- app/Http/Controllers/Api/Payment/VerifyController.php
+++ app/Http/Controllers/Api/Payment/VerifyController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api\Payment;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kind;
 use App\Models\Transaction;
+use App\Models\User;
+use App\Services\DonateAmountService;
 use App\Services\PaymentService;
 use App\Services\WalletService;
 use Illuminate\Http\Request;
@@ -17,19 +20,34 @@ class VerifyController extends Controller
             $payment = PaymentService::verifyPayment($request->Token, $transaction);
             
             if ($transaction->type == 'donate') {
-                WalletService::chargeWallet($transaction->streamer, $transaction->amount);
+                $streamer = $transaction->streamer;
+                $amountToBeCharge = $this->calculateAmount($streamer, $transaction);
+                WalletService::chargeWallet($streamer, $amountToBeCharge);
             } else if ($transaction->type == 'charge') {
                 WalletService::chargeWallet($transaction->user, $transaction->amount);
             } else if ($transaction->type == 'subscription') {
-                $vipPackagePrice = config('app.vip_package_price');
+                $vipPackagePrice = Kind::where('key', 'vip_package_price')->first()->value_2;
                 $months = $transaction->amount / $vipPackagePrice;
                 $transaction->user->assignRole('vip');
             }
 
             dd($payment);
+        }        
+        return 'error';
+    }
+
+    private function calculateAmount(User $streamer, Transaction $transaction): int
+    {
+        $amount = $transaction->raw_amount;
+        $finalAmount = $amount;
+        if (!$streamer->gateway->is_donator_pay_wage && $streamer->gateway->is_donator_pay_wage != '') {
+            $finalAmount -= DonateAmountService::calculateWage($streamer, $amount);
         }
 
-        
-        return 'error';
+        if (!$streamer->gateway->is_donator_pay_tax && $streamer->gateway->is_donator_pay_tax != '') {
+            $finalAmount -= DonateAmountService::calculateTax($amount);
+        }
+
+        return $finalAmount;
     }
 }
