776ebe19df4aeb41b2f6e8976ec4d10cc1bb535f
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-06-12 14:44:54 +0330

    chore: add get user credit in wallet service

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -6,7 +6,13 @@ use App\Models\User;
 
 class WalletService extends Service
 {
-    public static function chargeWallet(User $user, $amount)
+
+    public static function getUserCredit(User $user): int
+    {
+        return $user->wallet->credit;
+    }
+
+    public static function chargeWallet(User $user, int $amount)
     {
         $userWallet = $user->wallet;
         $userWallet->credit += $amount;
