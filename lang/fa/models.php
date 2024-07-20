18b32848127a9122cbb32cc474c640c3a7393bd2
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-07-20 17:16:10 +0330

    chore: add translation files

---
+++ lang/fa/models.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'models' => [
+        'user' => 'کاربران',
+    ],
+
+    'user' => [
+        'id' => 'شناسه',
+        'is_admin' => 'مدیر',
+        'first_name' => 'نام',
+        'last_name' => 'نام خانوادگی',
+        'mobile' => 'شماره تلفن',
+        'birthdate' => 'تاریخ تولد',
+        'gender' => 'جنسیت',
+        'email' => 'ایمیل',
+        'password' => 'رمز عبور'
+    ],
+];
