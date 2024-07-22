4d1cd76016c0c2d619bfa50b543ec2176a7de083
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-07-22 16:04:35 +0330

    chore: add profit migration and model

---
+++ database/migrations/2024_06_23_213140_create_profits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('payment_id')->nullable();
+            $table->bigInteger('amount_user_paid');
+            $table->bigInteger('amount_streamer_charged')->nullable();
+            $table->bigInteger('profit');
+            $table->timestamps();
+
+            $table->foreign('payment_id')->references('id')->on('payments')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profits');
+    }
+};
